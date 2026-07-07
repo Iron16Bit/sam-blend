6de06d0f063bf145618a7f5d65e87c8ce86209b3
Salvatore Gilles Cassarà <gilles2004.sc@gmail.com>
2026-07-07 15:53:34 +0200

    Fixed blend loop; Implementing blend packet structure

--- src/sam_blend/sam_blend copy.c
+++ src/sam_blend/sam_blend copy.c
@@ -29,27 +29,16 @@ LOG_MODULE_REGISTER(app_blend, CONFIG_LOG_DEFAULT_LEVEL);
 
 #include <hal/nrf_radio.h>
 
-static inline uint16_t blend_node_id(void)
-{
-    return (uint16_t)(NRF_FICR->DEVICEID[0] ^
-                      NRF_FICR->DEVICEID[1]);
-}
-
-static const uint8_t blend_protocol_id[5] = { 'B', 'L', 'E', 'N', 'D' };
-
 void sam_blend_loop(void *arg1, void *arg2, void *arg3) {
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
 
-    void (*cb)(struct net_buf *rx_buf) = (void (*)(struct net_buf *))arg1;
-    
-    SAM_MODULE_DATA_TYPE(core) *core_data =
-    SAM_MODULE_DEFAULT_INSTANCE_GET(core)->data;
-    
-    uint8_t channel = 37;
-    core_data->action_context.tx_delay = 0;
+    void (*cb)(void) = (void (*)(void))arg1;
 
-    LOG_INF("Starting BLEnd on node %u", blend_node_id());
+    SAM_MODULE_DATA_TYPE(core) *core_data =
+        SAM_MODULE_DEFAULT_INSTANCE_GET(core)->data;
+
+    uint8_t channel = 37;
 
 	while(1) {
         /* Start epoch timer */
@@ -70,26 +59,9 @@ void sam_blend_loop(void *arg1, void *arg2, void *arg3) {
 
         res = sam_core_rx(sa, &wa);
         if (res == SAM_SUCCESS) {
-            LOG_INF("RX len=%u", wa.rx_buf->len);
-
-            // Check if it is a BLEND packet
-
-            if (wa.rx_buf->len < 5) {
-                LOG_ERR("Received packet too short");
-                sam_buf_unref(wa.rx_buf);
-                wa.rx_buf = NULL;
-            } else if (memcmp(wa.rx_buf->data, blend_protocol_id, 5) != 0) {
-                LOG_ERR("Ignoring non-BLEnd packet");
-                sam_buf_unref(wa.rx_buf);
-                wa.rx_buf = NULL;
-            } else {
-                /* Remove the protocol identifier before passing the packet on */
-                net_buf_pull_mem(wa.rx_buf, 5);
-
-                (*cb)(wa.rx_buf);
-                sam_buf_unref(wa.rx_buf);
-                wa.rx_buf = NULL;
-            }
+            sam_buf_unref(wa.rx_buf);
+            wa.rx_buf = NULL;
+            (*cb)();
         } else if (res == SAM_TIMEOUT) {
             LOG_INF("No beacon received in the SCAN");
         } else {
@@ -103,23 +75,9 @@ void sam_blend_loop(void *arg1, void *arg2, void *arg3) {
             if (!res || buf == NULL) {
                 k_oops();
             }
-
-            // Data payload of BLEnd packet is empty at the moment
-            
-            // uint8_t msg[BLEND_PACKET_LEN];
-            // memset(&msg, 0, BLEND_PACKET_LEN);
-            // msg[0] = (uint8_t)'B';
-            // msg[1] = (uint8_t)'L';
-            // msg[2] = (uint8_t)'E';
-            // msg[3] = (uint8_t)'N';
-            // msg[4] = (uint8_t)'D';
-            // msg[5] = (uint8_t)NRF_FICR->DEVICEID[0];
-            // msg[6] = (uint8_t)NRF_FICR->DEVICEID[1];
-
-            // net_buf_push_mem(buf, &msg, sizeof(msg));
-
-            net_buf_add_u8(buf, i);
+            net_buf_add_u8(buf, i); //TODO add BLEnd packet structure
             sam_core_tx_enqueue(buf);
+            sam_buf_unref(buf); // we can unref (the core has now ownership)
 
             sam_tx_sched_args_t tsa = {};
             tsa.tx_buf = buf;
@@ -130,6 +88,8 @@ void sam_blend_loop(void *arg1, void *arg2, void *arg3) {
                 int random_slack_us = (rand() % RANDOM_SLACK_US);
                 k_sleep(K_USEC(random_slack_us));
             }
+
+            core_data->action_context.tx_delay = 0;
             res = sam_core_tx_sched(tsa, &last_op_id);
             if (!res) {
                 LOG_ERR("! sam_core_tx_sched returned %s", sam_get_std_result_name(res));
@@ -137,8 +97,7 @@ void sam_blend_loop(void *arg1, void *arg2, void *arg3) {
             }
 
             res = sam_core_tx_wait(last_op_id);
-            sam_buf_unref(buf); // we can unref (the core has now ownership)
-            k_sleep(K_USEC(TIMESLOT_REQUEST_DISTANCE_US-100));
+            k_sleep(K_USEC(TIMESLOT_REQUEST_DISTANCE_US));
             if (res != SAM_SUCCESS) {
                 LOG_ERR("Failed TX");
             }
@@ -162,7 +121,7 @@ K_THREAD_STACK_DEFINE(sam_blend_stack,
 
 static struct k_thread sam_blend_thread_data;
 
-void sam_start_blend(void (*cb)(struct net_buf *rx_buf))
+void sam_start_blend(void (*cb)(void))
 {
     k_thread_create(
         &sam_blend_thread_data,
